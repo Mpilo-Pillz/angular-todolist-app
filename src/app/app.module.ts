74b9f27e66990087c11d7ce93a1bfa639971a508
Mpilo Pillz <mpilopillz@Mpilos-MacBook-Pro.local>
2019-08-09 09:59:33 +0200

    Added a dynamic todo list component hard coded keeping hardcoded todo items as a guide as i prepare to add the service and model to implement observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListTitleComponent } from './list-title/list-title.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { AddToListComponent } from './to-do-list/add-to-list/add-to-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListTitleComponent,
-    ToDoListComponent
+    ToDoListComponent,
+    AddToListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/to-do-list/add-to-list/add-to-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-add-to-list',
+    templateUrl: './add-to-list.component.html',
+    styleUrls: ['./add-to-list.component.css']
+})
+
+export class AddToListComponent {
+
+}
+
